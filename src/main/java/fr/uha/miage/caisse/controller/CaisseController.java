943683178e37a8f59438e6be86424dc2f7f6a81c
MOHAMED <MOHAMED@MOHAMED-PC>
2014-11-19 13:19:42 +0100

    autocomplete avec en enregistrement dans la datagrid de la ligne de commande

--- src/main/java/fr/uha/miage/caisse/controller/CaisseController.java
+++ src/main/java/fr/uha/miage/caisse/controller/CaisseController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import fr.uha.miage.caisse.model.CategorieProduit;
+import fr.uha.miage.caisse.model.CommandeVirt;
 import fr.uha.miage.caisse.model.Produit;
 import fr.uha.miage.caisse.repository.CaisseRepository;
+import fr.uha.miage.caisse.repository.CommandeVirRepository;
 import fr.uha.miage.caisse.repository.ProduitRepository;
 
 
@@ -24,6 +26,9 @@ public class CaisseController {
 ProduitRepository prodRepository;
 @Autowired
 CaisseRepository caisseRepository;
+@Autowired
+CommandeVirRepository cmdVirRepository;
+
 	@RequestMapping(value = "/caisse", method = RequestMethod.GET)
 	public String getCaisse(Model model) {
 	
@@ -52,10 +57,34 @@ Produit json = (Produit) prodRepository.FIND_des(term);
 	String s=null;
 Produit p = (Produit) caisseRepository.FIND_quant(term);
 if(p.getQuantite()>quantite)
+{
+	CommandeVirt cmd= new CommandeVirt();
+	cmd.setCategorie(p.getCategorie().getDesignation());
+	cmd.setId(term);
+	cmd.setDesignation_pd(p.getDesignation_pd());
+	cmd.setPrix_ht(p.getPrix_ht());
+	cmd.setQuantite(quantite);
+	cmdVirRepository.save(cmd);
 s="passe";
+}
 else
 	s="nn";
-System.out.println(s);
+
 	return s;
+    }
+	
+	@ResponseBody
+	@RequestMapping(value = "/loaddatacmd", method = RequestMethod.POST,
+			
+			
+			headers="Accept=application/json")
+
+   
+
+    public List<CommandeVirt> modifier( Model model) {
+	
+List<CommandeVirt> json = (List<CommandeVirt>) cmdVirRepository.findAll() ;
+
+	return json;
     }
 }
---
+++ src/main/java/fr/uha/miage/caisse/repository/CommandeVirRepository.java
@@ -0,0 +1,18 @@
+package fr.uha.miage.caisse.repository;
+import org.springframework.data.repository.CrudRepository;
+
+import fr.uha.miage.caisse.model.CommandeVirt;
+
+	
+	
+
+
+
+
+
+	public interface CommandeVirRepository extends CrudRepository<CommandeVirt, Long> {
+
+	}
+
+
+
